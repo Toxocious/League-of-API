5b88583c31f206e980be7f147b38e2a0cbdaa738
Jesse Mack <Toxocious@gmail.com>
2020-03-23 16:50:24 -0700

    Query our node backend via our Summoner component.
    Most of the data that we'll be calling will be displayed in this Summoner component; this is just the baseline for now.

--- client/src/components/Summoner.js
+++ client/src/components/Summoner.js
@@ -11,18 +11,62 @@ export default class Summoner extends Component
 		super(props);
 
 		this.state = {
+			serverResponse: null,
+
 			summoner: {
-				Name: props.match.params.summoner,
-				Region: props.match.params.region,
+				name: props.match.params.summoner,
+				region: props.match.params.region,
 			}
 		};
 	}
 
+	/**
+	 * Fetch the data of the summoner that is being queried.
+	 * Passes both the desired region and summoner name in the URL.
+	 */
+  FetchAPI()
+  {
+		const safeParams = {
+			region: encodeURIComponent(this.state.summoner.region),
+			name: encodeURIComponent(this.state.summoner.name),
+		};
+
+    fetch(`http://localhost:9000/testAPI?region=${safeParams.region}&name=${safeParams.name}`)
+      .then(res => res.text())
+      .then(res => this.setState({ 
+        serverResponse: res
+      }))
+			.catch(err => err);
+	}
+	
+	/**
+	 * Upon mounting, retrieve data from our server regarding the desired summoner.
+	 */
+	componentDidMount()
+	{
+		this.FetchAPI();
+
+		console.log(this.state);
+	}
+
+	/**
+	 * Render our data.
+	 */
 	render()
 	{
 		return (
 			<div className='Summoner'>
-				<h2>{ this.state.summoner.Name }</h2>
+				<div className='Panel'>
+					<div className='Body'>
+						<div>{ this.state.serverResponse }</div>
+					</div>
+				</div>
+
+				<div className='Panel'>
+					<div className='Body'>
+						<div>Match history, Champion Mastery, etc.</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
