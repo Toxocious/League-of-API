814849e89912a648617d7b1e5d2517463ef2d196
Jesse Mack <Toxocious@gmail.com>
2020-03-23 16:49:25 -0700

    Setting a better foundation for the server-side code.
    Update the /testAPI path to be the first step towards utilizing a backend for the React application.

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,20 @@
+require('dotenv').config();
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+router.get("/", (req, res, next) =>
+{
+    const Region = req.query.region;
+    const SummonerName = req.query.name;
+
+    const Summoner = {
+        Name: SummonerName,
+        Region: Region,
+    };
+
+    //console.log("RIOT_API_KEY Environment Var: " + process.env.RIOT_API_KEY);
+
+    res.send(`Attempting to search for the summoner [${SummonerName}] in the region [${Region}].`);
 });
 
 module.exports = router;
